b367f667803b75be7429a3af915263cc5cfbe604
Alessio <abrits@gmail.com>
2025-06-27 22:05:30 +0200

    removing package lock

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,11 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
+import { vercelPreset } from "@vercel/remix/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
+installGlobals();
 
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    // Only include Remix plugin if not running tests
-    ...(process.env.VITEST ? [] : [
-      remix({
-        future: {
-          v3_fetcherPersist: true,
-          v3_relativeSplatPath: true,
-          v3_throwAbortReason: true,
-          v3_singleFetch: true,
-          v3_lazyRouteDiscovery: true,
-        },
-      }),
-    ]),
-    tsconfigPaths(),
-  ],
-  test: {
-    environment: "jsdom",
-    setupFiles: "./app/vitest.setup.ts",
-    globals: true,
-  },
-}));
+export default defineConfig({
+  plugins: [remix({ presets: [vercelPreset()] }), tsconfigPaths()],
+});
