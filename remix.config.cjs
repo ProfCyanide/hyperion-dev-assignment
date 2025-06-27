38f11f44f36f189c09f4388e9ef68bd37b2ee8dc
Alessio <abrits@gmail.com>
2025-06-27 14:16:45 +0200

    next try

--- remix.config.cjs
+++ remix.config.cjs
@@ -1,4 +1,4 @@
-   /** @type {import('@remix-run/dev').AppConfig} */
-   module.exports = {
-    serverBuildTarget: "vercel",
-  };
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  serverBuildTarget: "vercel",
+};
